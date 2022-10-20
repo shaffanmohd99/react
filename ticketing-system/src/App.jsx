b006b5ff867a33500f3114c2586bd6d31e9b110c
shaffanmohd <shaffanmohd@gmail.com>
2022-10-21 00:09:42 +0800

    remove Views in App.js

--- ticketing-system/src/App.jsx
+++ ticketing-system/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import Register from './Views/Register'
 import Login from './views/Login'
 import { Route, Routes } from 'react-router-dom'
 import NoMatch from './views/NoMatch'
